6c07b7215301bdef8912367dbb2d5aa5ab88be7c
Vaibhav Bedia <vaibhav.bedia@ti.com>
2013-05-08 14:26:03 +0530

    AM335X: CM3: Check SoC ID along with SoC Rev in DPLL code
    Instead of relying just on the SoC rev field check
    the SoC ID also. At the same time consolidate the
    DPLL power sequencing code and place it under proper
    checks.
    
    Signed-off-by: Vaibhav Bedia <vaibhav.bedia@ti.com>

--- src/pm_services/pm_handlers.c
+++ src/pm_services/pm_handlers.c
@@ -137,11 +137,7 @@ void a8_lp_cmd3_handler(struct cmd_data *data, char use_default_val)
 	interconnect_modules_disable();
 
 	/* DPLL retention update for PG 2.0 */
-	if (get_am335x_soc_rev() == AM335X_REV_ES2_0) {
-		dpll_power_down(DPLL_DDR);
-		dpll_power_down(DPLL_DISP);
-		dpll_power_down(DPLL_PER);
-	}
+	am33xx_power_down_plls();
 
 	mpu_clkdm_sleep();
 
@@ -151,7 +147,7 @@ void a8_lp_cmd3_handler(struct cmd_data *data, char use_default_val)
 	if (use_default_val || !(local_cmd->mosc_state)) {
 		disable_master_oscillator();
 		/* Core LDO retention for PG 2.0 if PD_PER is in RET */
-		if (get_am335x_soc_rev() == AM335X_REV_ES2_0) {
+		if (soc_id == AM335X_SOC_ID && soc_rev > AM335X_REV_ES1_0) {
 			if ((__raw_readl(AM335X_PM_PER_PWRSTST) &
 				PWR_STATE_STS_MASK) == POWER_STATE_STS_RET) {
 				/* set Auto_RAMP_EN in SMA2 Spare Register (SMA2). */
@@ -221,11 +217,7 @@ void a8_lp_cmd5_handler(struct cmd_data *data, char use_default_val)
 	pd_state_change(per_st, PD_PER);
 
 	/* DPLL retention update for PG 2.0 */
-	if(get_am335x_soc_rev() == AM335X_REV_ES2_0) {
-		dpll_power_down(DPLL_DDR);
-		dpll_power_down(DPLL_DISP);
-		dpll_power_down(DPLL_PER);
-	}
+	am33xx_power_down_plls();
 
 	mpu_clkdm_sleep();
 
@@ -284,11 +276,7 @@ void a8_lp_cmd7_handler(struct cmd_data *data, char use_default_val)
 	pd_state_change(per_st, PD_PER);
 
 	/* DPLL retention update for PG 2.0 */
-	if (get_am335x_soc_rev() == AM335X_REV_ES2_0) {
-		dpll_power_down(DPLL_DDR);
-		dpll_power_down(DPLL_DISP);
-		dpll_power_down(DPLL_PER);
-	}
+	am33xx_power_down_plls();
 
 	wkup_clkdm_sleep();
 
@@ -426,7 +414,7 @@ void a8_wake_cmd3_handler(void)
 {
 	int result = 0;
 
-	if (get_am335x_soc_rev() == AM335X_REV_ES2_0)
+	if (soc_id == AM335X_SOC_ID && soc_rev > AM335X_REV_ES1_0)
 		core_ldo_power_up();
 
 	result = verify_pd_transitions();
@@ -437,12 +425,7 @@ void a8_wake_cmd3_handler(void)
 
 	clkdm_wake();
 
-	/* DPLL retention update for PG 2.0 */
-	if (get_am335x_soc_rev() == AM335X_REV_ES2_0) {
-		dpll_power_up(DPLL_DDR);
-		dpll_power_up(DPLL_DISP);
-		dpll_power_up(DPLL_PER);
-	}
+	am33xx_power_up_plls();
 
 	interconnect_modules_enable();
 
@@ -471,12 +454,7 @@ void a8_wake_cmd5_handler(void)
 
 	wkup_clkdm_wake();
 
-	/* DPLL retention update for PG 2.0 */
-	if (get_am335x_soc_rev() == AM335X_REV_ES2_0) {
-		dpll_power_up(DPLL_DDR);
-		dpll_power_up(DPLL_DISP);
-		dpll_power_up(DPLL_PER);
-	}
+	am33xx_power_up_plls();
 
 	essential_modules_enable();
 
@@ -503,12 +481,7 @@ void a8_wake_cmd7_handler(void)
 
 	wkup_clkdm_wake();
 
-	/* DPLL retention update for PG 2.0 */
-	if (get_am335x_soc_rev() == AM335X_REV_ES2_0) {
-		dpll_power_up(DPLL_DDR);
-		dpll_power_up(DPLL_DISP);
-		dpll_power_up(DPLL_PER);
-	}
+	am33xx_power_up_plls();
 
 	msg_cmd_stat_update(result);
 
