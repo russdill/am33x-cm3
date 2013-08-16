c77c19a299116835c0fee29581b6ff326b1d8a8e
Russ Dill <Russ.Dill@ti.com>
2013-08-16 16:52:12 -0700

    Fixup ext31_handler cleanup

--- src/foundation/ext_intr_handlers.c
+++ src/foundation/ext_intr_handlers.c
@@ -32,11 +32,11 @@ void extint31_handler(void)
 {
 	nvic_disable_irq(AM335X_IRQ_MBINT0);
 
-	/*
-	 * If command is not valid, need to update the status to FAIL
-	 * and enable the mailbox interrupt back
-	 */
 	if (!msg_cmd_is_valid()) {
+		/*
+		 * If command is not valid, need to update the status to FAIL
+		 * and enable the mailbox interrupt back
+		 */
 		msg_cmd_stat_update(CMD_STAT_FAIL);
 
 	} else if (msg_cmd_needs_trigger()) {
